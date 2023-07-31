1d098ec92a3fc94b6a51f9130a7fd835ead12cfc
Victor Sacek <sacek@usp.br>
2023-07-31 11:28:39 -0300

    change gitignore and location for make.sh

---
+++ make.sh
@@ -0,0 +1,6 @@
+cd src
+g++ aloca_difusao.cpp aloca_topo_din.cpp aloca_uplift.cpp aloca_falha_3D.cpp aloca_falhas_1.cpp aloca_fluvi.cpp aloca_topo.cpp calc_Temper35.cpp calcula_IsoT.cpp calor.cpp desloca_tudo.cpp desloca.cpp difusao_linear.cpp fault_analit.cpp fault3D.cpp flexura_load.cpp flexura_solv.cpp flexuraK.cpp fluvi_min.cpp fluvi.cpp free_all.cpp gera_malha_parc.cpp gera_malha.cpp lateral.cpp main.cpp malha_regular.cpp Malha.cpp modif_moho.cpp modif_topo.cpp monta_falha.cpp monta_h_w.cpp topo_din.cpp Orog_varios_uplift.cpp perturb_thermal.cpp print_topo.cpp re_voronoi.cpp stream.cpp thermal_aloca.cpp thermal_createf.cpp thermal_firstK1.cpp thermal_inic.cpp thermal_interp.cpp thermal_modiftopo.cpp thermal_montaK.cpp thermal_renew.cpp thermal_solv.cpp var_fault_stream.cpp var_fault.cpp vR_calc.cpp vR_sort.cpp -o ../bin/scifom -Wall
+
+
+
+
